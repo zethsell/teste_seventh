13420a8d389969391bae6e7a8d58784b0be77464
zeth_sell <marcio.theo.92@gmail.com>
2021-12-09 16:34:13 -0300

    correcao migration

--- backend/database/migrations/2021_12_08_213044_add_level_id_user_table_create_level_table.php
+++ backend/database/migrations/2021_12_08_213044_add_level_id_user_table_create_level_table.php
@@ -20,7 +20,7 @@ class AddLevelIdUserTableCreateLevelTable extends Migration
         });
 
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('level_id');
+            $table->foreignId('level_id')->default('');
 
             $table->foreign('level_id')->references('id')->on('levels');
         });
